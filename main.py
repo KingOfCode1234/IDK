985a58240a61593f75b12840820291fa89c9e2e7
KingOfCode1234 <132614154+KingOfCode1234@users.noreply.github.com>
2023-05-04 16:09:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,65 +1,50 @@
-import multiprocessing
-import wave
-import pyaudio
+import os
+import importlib.util
+import json
 
-print("Importing DONE")
+# Load the JSON file
+json_file_path = "plugins/config.json"
+with open(json_file_path) as json_file:
+    try:
+        data = json.load(json_file)
+    except json.JSONDecodeError as e:
+        print(f"Error parsing JSON file: {e}")
+        exit(1)
 
-class Music:
-    def __init__(self):
-        self.music_list_LOAD = ["Plateau.wav", "compiled.wav"]
-        self.LOAD_next = iter(self.music_list_LOAD)
-        self.Process_Music = None
-        self.current_audio = ""
-        self.p = None
+directory = "plugins"
 
-    def stop(self):
-        if self.Process_Music is not None:
-            self.Process_Music.terminate()
-            self.Process_Music = None
-            if self.p is not None:
-                self.p.terminate()
-                self.p = None
-
-    def next(self, next_audio):
-        self.stop()
-        self.current_audio = next_audio
-        self.Process_Music = multiprocessing.Process(target=self.play_process, name="Music")
-        self.Process_Music.start()
-              
-    def get_from_directory(self, directory):
-        import os
-        for root, dirs, files in os.walk('music', topdown=True):
-            self.music_list_LOAD.append(files)
-            
+# Load each module, instantiate a class, and call a method from it
+for item in data:
+    module_name = item["module"]
+    module_path = os.path.join(directory, module_name + ".py")
+    if not os.path.exists(module_path):
+        print(f"Module {module_name} not found at {module_path}")
+        continue
+    
+    spec = importlib.util.spec_from_file_location(module_name, module_path)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    
+    if "class" in item:
+        class_name = item["class"]
+        if not hasattr(module, class_name):
+            print(f"Class {class_name} not found in module {module_name}")
+            continue
         
-    def play(self):
-        self.p = pyaudio.PyAudio()
-        wf = wave.open(self.current_audio, 'rb')
-        stream = self.p.open(format=self.p.get_format_from_width(wf.getsampwidth()),
-                             channels=wf.getnchannels(),
-                             rate=wf.getframerate(),
-                             output=True)
-        data = wf.readframes(1024)
-        while data:
-            stream.write(data)
-            data = wf.readframes(1024)
-        wf.close()
-        stream.stop_stream()
-        stream.close()
-
-    def _play_process(self):
-        self.play()
-
-    def start(self):
-        self.current_audio = next(self.LOAD_next)
-        self.Process_Music = multiprocessing.Process(target=self._play_process, name="Music")
-        self.Process_Music.start()
-        print(self.current_audio)
+        class_ = getattr(module, class_name)
+        instance = class_(**item.get("args", {}))
+        if hasattr(instance, "method_name"):
+            result = instance.method_name("argument1", "argument2")
+        else:
+            print(f"Method method_name not found in class {class_name} of module {module_name}")
+            continue
+    else:
+        function_name = item.get("function", "function_name")
+        if not hasattr(module, function_name):
+            print(f"Function {function_name} not found in module {module_name}")
+            continue
         
-        while self.Process_Music is not None and self.Process_Music.is_alive():
-            user = input("Say stop to quit:")
-            if "stop" == user:
-                self.stop()
-                break
-            if "next" == user:
-                self.next(next(self.LOAD_next))
+        result = getattr(module, function_name)(**item.get("args", {}))
+    
+    print(f"Result from {module_name}: {result}")
+      
